6d6e590202b66198d4ce6d86aed1bad508dc8c82
Cloudy yu <geekityu.cn@gmail.com>
2025-10-03 23:31:36 +0800

    公共字段自动填充利用Aop视线（有点难感觉）

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,7 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -19,7 +22,8 @@ public interface EmployeeMapper {
     /**
      * 插入员工数据
      */
-    @Select("insert into employee (username, name, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)" +
+    @AutoFill(value = OperationType.INSERT)
+    @Insert("insert into employee (username, name, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)" +
             " values (#{username}, #{name}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
 
@@ -34,6 +38,7 @@ public interface EmployeeMapper {
      * @param employee
      * @return
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**

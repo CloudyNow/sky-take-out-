d4fe4bf0c7dd0bde1b2f7a8d7c00644aca1bf180
Cloudy yu <geekityu.cn@gmail.com>
2025-10-26 22:48:43 +0800

    工作台业务代码业务开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
@@ -106,4 +107,11 @@ public interface DishMapper {
      * @return
      */
     List<Dish> getBySetmealId(Long id);
+
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
